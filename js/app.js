f2e293b726fdb5d5aca32f505a0902bd16e3a88f
Julie Blydenburgh <juliebly@gmail.com>
2013-04-09 21:10:45 -0400

    Cookie monster

---
+++ js/app.js
@@ -0,0 +1,8 @@
+$(function(event){
+
+	$("#startSwapping").click(function(event){
+		$("body").css("background-image", "url(\"images/bg1.jpg\")");
+		$("h2").html("Peanut Butter Cookie Evanglist");
+	});
+
+});
